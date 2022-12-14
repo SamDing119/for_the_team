e2269765b29fec127fd862d91d94450e7d2614e2
SamDing119 <61504510+SamDing119@users.noreply.github.com>
2022-12-14 18:51:16 +0800

    1214

--- js/web.js
+++ js/web.js
@@ -1,91 +1,106 @@
-        gsap.to("#W1",
-        {
-            x: -1100,
-            scrollTrigger:
-            {
-             start: "10% 15%",
-             end: "30% 20%",
-             scrub: true,
-             markers: false,
-            }
-        })
+gsap.to("#W1",
+    {
+        x: -1100,
 
-        gsap.to("#W2",
+        scrollTrigger:
         {
-            x: 1100,          
-            scrollTrigger:
-            {
-             start: "10% 15%",
-             end: "30% 20%",
-             scrub: true,
-             markers:false,
-            }
-        })
+            trigger: "#title",
+            start: "top 10px",
+            end: "bottom 20px",
+            scrub: true,
+            markers: true,
+        }
+    })
 
-        gsap.to("#GN",
+gsap.to("#W2",
+    {
+        x: 1100,
+        scrollTrigger:
         {
-            scale: 1.1,
-            y: 90,
-            scrollTrigger:
-            {
-             start: "10% 15%",
-             end: "30% 20%",
-             scrub: true,
-             markers:false,
-            }
-        })
+            trigger: "#title",
+            start: "top 10px",
+            end: "bottom 20px",
+            scrub: true,
+            markers: true,
+        }
+    })
 
-        gsap.to("#Boy",
+gsap.to("#GN",
+    {
+        scale: 1.1,
+        y: 90,
+        scrollTrigger:
         {
-            scale: 0.6,
-            x: 80,
-            y: 130, 
-            //Transformorigin:"100% 100%",
-            trigger: "#Boy",
-            scrollTrigger:
-            {
-             start: "10% 15%",
-             end: "30% 20%",
-             scrub: true,
-             markers:false,
-            }
-        })       
+            trigger: "#title",
+            start: "top 10px",
+            end: "bottom 20px",
+            scrub: true,
+            markers: true,
+        }
+    })
 
-        gsap.to("#WW3",
+gsap.to("#Boy",
+    {
+        scale: 0.6,
+        x: 80,
+        y: 130,
+        //Transformorigin:"100% 100%",
+        trigger: "#Boy",
+        scrollTrigger:
         {
-            x: -500,
-            scrollTrigger:
-            {
-             start: "10% 15%",
-             end: "30% 20%",
-             scrub: true,
-             markers:false,
-            }
-        })
+            start: "20% 30%",
+            end: "30% 20%",
+            scrub: true,
+            markers: true,
+        }
+    })
 
-        gsap.to("#HD",
+gsap.to("#WW3",
+    {
+        x: -500,
+        scrollTrigger:
         {
-            scale: 0.9,
-            x: -150,
-            opacity:1,
-            scrollTrigger:
-            {
-             start: "15% 15%",
-             end: "30% 20%",
-             scrub: true,
-             markers:false,
-            }
-        })       
+            trigger: "#title",
+            start: "top 10px",
+            end: "bottom 20px",
+            scrub: true,
+            markers: true,
+        }
+    })
 
-        gsap.to("#title",
+gsap.to("#top",
+    {
+        scale: 0.9,
+        x: -150,
+        opacity: 1,
+        scrollTrigger:
         {
-            scale: 0.8,
-            y: 150,
-            scrollTrigger:
-            {
-             start: "10% 15%",
-             end: "30% 20%",
-             scrub: true,
-             markers:false,
-            }
-        })
+            trigger: "#title",
+            start: "top 10px",
+            end: "bottom 20px",
+            scrub: true,
+            markers: true,
+        }
+    })
+
+gsap.to("#title",
+    {
+        scale: 0.8,
+        y: 150,
+        scrollTrigger:
+        {
+            trigger: "#title",
+            start: "top 10px",
+            end: "bottom 20px",
+            scrub: true,
+            markers: true,
+        }
+    })
+ScrollTrigger.matchMedia({
+    // 跟設定css 一樣  如果畫面不小於 992px 執行
+    "(min-width: 992px)": () => { 執行內容 },
+    // 畫面不小於 768px 執行
+    "(min-width: 768px)": () => { 執行內容 },
+    // 不管畫面大小，我都執行
+    "all": () => { 執行內容 },
+});
